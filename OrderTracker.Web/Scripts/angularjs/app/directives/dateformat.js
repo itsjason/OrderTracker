ff45c2bb81ab00c7b7c4416cbe997627d6350ffb
ghCoder <ahmad.ghaznawi@fcsamerica.com>
2013-10-02 13:03:44 -0500

    custom directive (dateformat.js) for InvoiceDetails (InvoiceDate)

---
+++ OrderTracker.Web/Scripts/angularjs/app/directives/dateformat.js
@@ -0,0 +1,30 @@
+﻿angular.module('orderTrackerApp')
+.directive('ngDateFormat', function ($filter) {
+
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+
+        link: function (scope, element, attrs, ctrl) {
+            ctrl.$formatters.unshift(function (modelValue) {
+                // To be diplayed to the user;
+
+                if (modelValue) {
+                    modelValue = $filter('moment')(modelValue, 'MM/DD/YYYY');
+                }
+
+                return modelValue;
+            });
+
+            ctrl.$parsers.unshift(function (viewValue) {
+                // To be added back into the model
+
+                if (viewValue) {
+                    viewValue = $filter('moment')(viewValue, 'MM/DD/YYYY');
+                }
+
+                return viewValue;
+            });
+        }
+    };
+});
